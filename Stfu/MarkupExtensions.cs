5be2e829c3a451de31e4bcdf0f282887e694c65b
Sam Hocevar <sam@hocevar.net>
2024-09-23 11:14:28 +0200

    Add a configurable BoolToVisibilityConverter for XAML

--- Stfu/MarkupExtensions.cs
+++ Stfu/MarkupExtensions.cs
@@ -1,7 +1,7 @@
 //
 //  Stfu — Sam’s Tiny Framework Utilities
 //
-//  Copyright © 2017–2021 Sam Hocevar <sam@hocevar.net>
+//  Copyright © 2017–2024 Sam Hocevar <sam@hocevar.net>
 //
 //  This library is free software. It comes without any warranty, to
 //  the extent permitted by applicable law. You can redistribute it
@@ -12,6 +12,7 @@
 
 using System;
 using System.Globalization;
+using System.Windows;
 using System.Windows.Data;
 using System.Windows.Markup;
 
@@ -27,5 +28,23 @@ namespace Stfu.Wpf
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
             => value is bool b ? !b : value;
+    }
+
+    /// <summary>
+    /// Convert a boolean to a visibility value suitable for XAML objects.
+    /// </summary>
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    public sealed class BoolToVisibilityConverter : MarkupExtension, IValueConverter
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider) => this;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => value is bool ? (bool)value ? IfTrue : IfFalse : null;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => Equals(value, IfTrue) ? true : Equals(value, IfFalse) ? false : null;
+
+        public Visibility IfTrue { get; set; } = Visibility.Visible;
+        public Visibility IfFalse { get; set; } = Visibility.Collapsed;
     }
 }
